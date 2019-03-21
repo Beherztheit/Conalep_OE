aa5c6d74b3251dd0adaa04e2c32093eaec3edf1d
alemaricame <31997262+alemaricame@users.noreply.github.com>
2019-03-20 18:56:08 -0600

    examen.php/examen.css

--- examen.php
+++ examen.php
@@ -8,30 +8,6 @@
 	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
 	<meta name="author" content="QBOOTSTRAP.COM" />
 
-  <!-- 
-	//////////////////////////////////////////////////////
-
-	FREE HTML5 TEMPLATE 
-	DESIGNED & DEVELOPED by QBOOTSTRAP.COM
-		
-	Website: 		http://qbootstrap.com/
-	Email: 			info@qbootstrap.com
-	Twitter: 		http://twitter.com/Q_bootstrap
-	Facebook: 		https://www.facebook.com/Qbootstrap
-	//////////////////////////////////////////////////////
-	 -->
-
-  	<!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
-
 	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700" rel="stylesheet">
 	
@@ -50,6 +26,8 @@
 	<link rel="stylesheet" href="css/flexslider.css">
 	<!-- Theme style  -->
 	<link rel="stylesheet" href="css/style.css">
+    <!-- Examen stilos -->
+    <link rel="stylesheet" href="css/examen.css">
 
 	<!-- Modernizr JS -->
 	<script src="js/modernizr-2.6.2.min.js"></script>
@@ -58,41 +36,25 @@
 	<script src="js/respond.min.js"></script>
 	<![endif]-->
 
-	</head>
-	<body>
-		
-	<div class="qbootstrap-loader"></div>
-	
-	<div id="page">
-	<nav class="qbootstrap-nav" role="navigation">
-		<!-- -->
+    </head>
+    
+    <header>
+        <div class="exam">
+            <h1>Examen<h1>
+        </div>
+    </header>
 
-			<footer id="qbootstrap-footer" role="contentinfo">
+    <body>
+        <div class="instrucciones">Lee con atención cada una de las preguntas y escoge la opción que consideres adecuada. 
+            Para resolver el examen tienes un tiempo máximo de 50 minutos. 
+            No se requiere ningún instrumento (formulario, calculadora o tabla periódica).
+        </div>
+        <center><label id = "tiempo">50:00</label></center>
 
-				
-			</footer> 
-
-<!-- -->
-		<div class="top-menu">
-			
-			<div class="container">
-
-				<div class="row">
-					<div class="col-xs-12">
-						<div class="top">
-							<div class="row">
-								
-								
-					
-					<div class="col-md-6 col-md-offset-3 text-center qbootstrap-heading"><h2><label id = "tiempo">50:00</label></h4></div>
-						
-				</div>
-				<!--Validar preguntas-->
-				<form data-toggle="validator" role="form">
-					<!-- -->
-				<div class="form-group" id="preguntas">
+        <div class="preguntas">
+        <form role="form">
 				<h4>1.- Son los sistemas de medidas que se usan comúnmente en las herramientas.</h4>
-				<div class="checkbox" id="1">
+				<div  id="1">
 					<label class="checkbox-inline">
 				    <input type="checkbox" id="inlineCheckbox1" value="option1" required>
 				    	a) Sistema decimal e inglés.
@@ -108,7 +70,7 @@
 				</div>
 				<!-- -->
 				<h4>2.- Son las medidas de matracas, manerales, dados, extensiones y accesorios.</h4>
-				<div class="checkbox" id="2">
+				<div id="2">
 				    <input type="checkbox" id="checkbox1" value="option1">
 				    	a) a. 5/8, 13/16, 1/4 y 3/8.  
 				    	<br>
@@ -120,7 +82,7 @@
 				</div>
 				<!-- -->
 				<h4>3.- Son las herramientas para quitar y poner las bujías de 5/8 de un vehículo. </h4>
-				<div class="checkbox" id="3">
+				<div id="3">
 				    <input type="checkbox" id="checkbox1" value="option1">
 				    	a) Dado 5/8 para bujías, matraca 3/8 ,extensión larga 3/8 y calibrador de bujías.   
 				    	<br>
@@ -132,7 +94,7 @@
 				</div>
 				<!-- -->
 				<h4>4.- Son las herramientas para desarmar y armar una cabeza de motor con tornillos estándar.</h4>
-				<div class="checkbox" id="4">
+				<div  id="4">
 				    <input type="checkbox" id="checkbox1" value="option1">
 				    	a) Dados estándar y accesorios, torquimetro, juego de desarmadores, llaves allen, pinzas y martillos.
 				    	<br>
@@ -144,7 +106,7 @@
 				</div>
 				<!-- -->
 				<h4>5.- Herramienta de medición que realiza mediciones de exteriores, interiores profundidad.</h4>
-				<div class="checkbox" id="5">
+				<div  id="5">
 				    <input type="checkbox" id="checkbox1" value="option1">
 				    	a) Micrómetro.
 				    	<br>
@@ -156,7 +118,7 @@
 				</div>
 				<!-- -->
 				<h4>6.- Forma y color de los señalamientos de prohibición.</h4>
-				<div class="checkbox" id="6">
+				<div  id="6">
 				    <input type="checkbox" id="checkbox1" value="option1">
 				    	a) Triangulo, amarillo y negro.
 				    	<br>
@@ -168,7 +130,7 @@
 				</div>
 				<!-- -->
 				<h4>7.- Forma y color de los señalamientos obligación.</h4>
-				<div class="checkbox" id="7">
+				<div  id="7">
 				    <input type="checkbox" id="checkbox1" value="option1">
 				    	a) Triangulo, amarillo y negro.
 				    	<br>
@@ -180,7 +142,7 @@
 				</div>
 				<!-- -->
 				<h4>8.- Forma y color de los señalamientos advertencia.</h4>
-				<div class="checkbox" id="8">
+				<div  id="8">
 				    <input type="checkbox" id="checkbox1" value="option1">
 				    	a) Triangulo, amarillo y negro.
 				    	<br>
@@ -192,7 +154,7 @@
 				</div>
 				<!-- -->
 				<h4>9.- Forma y color de los señalamientos de vías de evacuación.</h4>
-				<div class="checkbox" id="9">
+				<div  id="9">
 				    <input type="checkbox" id="checkbox1" value="option1">
 				    	a) Cuadrado o rectángulo, verde y blanco.
 				    	<br>
@@ -204,7 +166,7 @@
 				</div>
 				<!-- -->
 				<h4>10.- Forma y color de los señalamientos contra incendios.</h4>
-				<div class="checkbox" id="10">
+				<div  id="10">
 				    <input type="checkbox" id="checkbox1" value="option1">
 				    	a) Cuadrado o rectángulo, verde y blanco.
 				    	<br>
@@ -214,51 +176,41 @@
 			  			<input type="checkbox" id="checkbox3" value="option3">
 			  			c) Círculo, azul y blanco.
 				</div>
-
-
-			</div><!--Termina div de preguntas -->
-				
+				<br>
 				<!-- Boton para terminar examen-->
-				<center><a href="index.html"><button type="button" class="btn btn-success" id="fin">Terminar.</button></a></center>
-				</div>	
-							</form>	
-							</div>
-						</div>
-					</div>
-					
-							</div>
-					</div>
-		</div>
-	</nav>
-
+				<center><a href="index.html"><button type="button" class="btn btn-success" id="fin">Terminar.</button></a></center>	
+			</form>	
+						
+</div>
 	<div class="gototop js-top">
 		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
 	</div>
-	
-	<!-- jQuery -->
-	<script src="js/jquery.min.js"></script>
-	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
-	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
-	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
-	<!-- Stellar Parallax -->
-	<script src="js/jquery.stellar.min.js"></script>
-	<!-- Carousel -->
-	<script src="js/owl.carousel.min.js"></script>
-	<!-- Flexslider -->
-	<script src="js/jquery.flexslider-min.js"></script>
-	<!-- countTo -->
-	<script src="js/jquery.countTo.js"></script>
-	<!-- Magnific Popup -->
-	<script src="js/jquery.magnific-popup.min.js"></script>
-	<script src="js/magnific-popup-options.js"></script>
-	<!-- Sticky Kit -->
-	<script src="js/sticky-kit.min.js"></script>
-	<!-- Main -->
-	<script src="js/main.js"></script>
+    </body>
 
-	</body>
-</html>
+    </html>
 
+
+    <!--Contador-->
+    <script type="text/javascript">
+		function redireccionar(){
+			window.location.href="Segundoexamen.html";
+			alert("Haz llegado al tiempo límite.");
+		}
+
+			var salida = document.getElementById("tiempo"),
+				minutos = 50,
+				segundos = 0,
+				intervalo = setInterval(function(){
+					if (--segundos < 0){
+					    segundos = 59;
+					    minutos--;
+					    setTimeout("redireccionar()",3000000); //50 min - 3,000,000 ms
+
+					}
+					      
+					if (!minutos && !segundos)
+					    clearInterval(intervalo);
+					    salida.innerHTML = minutos + ":" + (segundos < 10 ? "0" + segundos : segundos);
+					}, 1000);
+    </script>
+       <!-- fin del contador -->
