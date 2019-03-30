591dd8b3562546f47fba6ae7782a896ceed6cbc9
Eduardo <beatdrachen@gmail.com>
2019-03-30 07:35:45 -0600

    cambios

---
+++ getdata.php
@@ -0,0 +1,17 @@
+<?php
+    include("conexion.php");
+    
+    $nombre =;
+    $apellidop =;
+    $apellidom =;
+    $grado =;
+    $grupo =;
+    $query="insert into registro";
+    $resultado =$conexion->query($query);
+
+    if(){
+        include("examen.php");
+    }else{
+        echo "Queue";
+    }
+?>
