0aef2012fe551d59c7f663cf249f7eb7b4fcf6dd
alemaricame <31997262+alemaricame@users.noreply.github.com>
2019-05-08 18:52:53 -0500

    perfil_prof

--- perfil_prof.php
+++ perfil_prof.php
@@ -1,12 +1,41 @@
 <!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>EXAMEN</title>
+	<title>CONALEP</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Free HTML5 Template by QBOOTSTRAP.COM" />
 	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
 	<meta name="author" content="QBOOTSTRAP.COM" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+  <!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by QBOOTSTRAP.COM
+		
+	Website: 		http://qbootstrap.com/
+	Email: 			info@qbootstrap.com
+	Twitter: 		http://twitter.com/Q_bootstrap
+	Facebook: 		https://www.facebook.com/Qbootstrap
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
 
 	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700" rel="stylesheet">
@@ -27,7 +56,8 @@
 	<!-- Theme style  -->
 	<link rel="stylesheet" href="css/style.css">
     <!-- Examen stilos -->
-    <link rel="stylesheet" href="css/examen.css">
+	<link rel="stylesheet" href="css/examen.css">
+	<link rel="stylesheet" href="css/profesor.css">
 
 	<!-- Modernizr JS -->
 	<script src="js/modernizr-2.6.2.min.js"></script>
@@ -37,15 +67,63 @@
 	<![endif]-->
 
     </head>
-    
     <header>
         <div class="exam">
-            <h1>Perfil<h1>
+            <h1>Examen<h1>
+            <a style="float:right" href="login.php"><i class="material-icons">exit_to_app</i></a>
         </div>
+
     </header>
+	<body>
+		
+	<div class="qbootstrap-loader"></div>
+    <div class="datos">
 
-    <body>
+    <h3>Datos generales del profesor</h3>
+    <hr>
+        <label for="inputName"class="control-label" >Nombre del profesor: </label>
+        <input type="text" class="form-control" id="profesor" placeholder="Nombre del profesor" disabled>
+		<label for="inputName"class="control-label" >Matricula: </label>
+        <input type="text" class="form-control" id="matricula" placeholder="Matricula" disabled>
+		<hr>
+		<button class="btn btn-info">Editar</button>
+    </div>
 
-    </body>
+	<div class="clases">
+		<h3>Materias y grupos</h3>
+		<hr>
+			<li class="class">Materias</li>
+	</div>
 
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Flexslider -->
+	<script src="js/jquery.flexslider-min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Sticky Kit -->
+	<script src="js/sticky-kit.min.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+	<script src="js/examen.js"></script>
+
+	</body>
 </html>
+
